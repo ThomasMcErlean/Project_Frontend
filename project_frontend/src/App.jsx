c6f8ce5eca792631ed59d7f4df0af0992a1eb88d
Tom McErlean <TM1000@live.mdx.ac.uk>
2026-04-01 17:25:56 +0100

    Added dashboard

--- project_frontend/src/App.jsx
+++ project_frontend/src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './index.css'
 import Accountcreate from './pages/AccountCreate'
+import Dashboard from './pages/Dashboard'
 import {
   BrowserRouter as Router,
-    Route,
+  Route,
   NavLink,
-  Routes
+  Routes,
+  Outlet
 } from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
@@ -16,125 +18,43 @@ function App() {
   return (
     <>
       <section id="center" >
-        <header>
-          <h1>Website</h1>
-        </header>
+
         <Router>
-        <nav>
-          <ul>
-            <li>
-              <NavLink 
-                exact to="/AccountCreation" 
-                activeClassName="selected">
-                Account Create
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+          <header>
+            <h1>Website</h1>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink
+                    exact to="/AccountCreation"
+                    activeClassName="selected">
+                    Account Create
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    exact to="/Dashboard"
+                    activeClassName="selected">
+                    Dashboard
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </header>
           <Routes>
-           <Route
-                    exact path="/AccountCreation"
-                    element={<Accountcreate />}
-                />
+            <Route
+              exact path="/AccountCreation"
+              element={<Accountcreate />}
+            />
+            <Route
+              exact path="/Dashboard"
+              element={<Dashboard />}
+            />
           </Routes>
-      </Router>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="button-primary"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        </Router>
       </section>
 
       <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
---
+++ project_frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <>
+      <div className="dashboard-grid-container">
+        <div className="sidebar"></div>
+      </div>
+
+    </>)
+}
+
+export default Dashboard;
